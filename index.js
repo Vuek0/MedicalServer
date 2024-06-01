fefbf51fcdcd0641b8d21ce5ae5cbbb7d904a179
Vuek0 <vueko.eruko@gmail.com>
2024-06-01 18:52:41 +0500

    create the server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+
+module.exports = app;
