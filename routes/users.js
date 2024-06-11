8d184d5474953f55c3a19cbabffb848abb92d3d9
Vuek0 <vueko.eruko@gmail.com>
2024-06-11 18:07:47 +0500

    env

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,21 @@ const express = require("express"),
 const fs = require('fs');
 const path = require('path');
 const data = path.join(__dirname.split('\\routes')[0], '/users.json')
+require('dotenv').config()
 router.route('/').get((req, res) => {
-    console.log(data);
+    console.log(req.query.key);
     fs.readFile(data, 'utf-8', (error, data) => {
         res.json(JSON.parse(data));
     })
+}).post((req, res) => {
+    if(req.query.key === process.env.API_KEY){
+        res.send("posted bro")
+    }else if(req.query.key !== process.env.API_KEY){
+        res.send("Invalid Api Key");
+    }else{
+        res.send("Api Key is required")
+    }
+    
 })
 
 module.exports = router;
