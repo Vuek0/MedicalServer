8aced73b18b406cd5b1fc70ad0d47e7be49eeab9
Vuek0 <vueko.eruko@gmail.com>
2024-06-11 17:55:30 +0500

    yes

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const users = require("./routes/users");
 const PORT = process.env.PORT || 3333;
-console.log(process.env);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,12 @@ const express = require("express"),
     router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const data = path.join(__dirname.split('\\routes')[0], '/users.json')
 router.route('/').get((req, res) => {
-    fs.readFile()
-    res.send('get users');
+    console.log(data);
+    fs.readFile(data, 'utf-8', (error, data) => {
+        res.json(JSON.parse(data));
+    })
 })
 
 module.exports = router;
